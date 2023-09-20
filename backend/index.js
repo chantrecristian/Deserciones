6b08473d28ddbed88943e102eebaec9c46833728
Cristian <chantrecristian2@gmail.com>
2023-09-20 09:40:04 -0600

    creacion de aplicativo deserciones

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+//const express = require("express");
+import express from "express";
+
+const app = express();
+
+app.listen(4000, () =>{
+    console.log("Servidor corriendo en el puerto 4000")
+})
